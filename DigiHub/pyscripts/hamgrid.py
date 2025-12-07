f50f686442989a8e75dff712e0d77657acc3e084
debods <57447490+debods@users.noreply.github.com>
2025-12-07 08:25:54 -0500

    Initial commit

--- DigiHub/pyscripts/hamgrid.py
+++ DigiHub/pyscripts/hamgrid.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
 
+import math, sys
+
+def latlon_to_grid(latitude, longitude):
+ field_lon = int((longitude + 180) / 20)
+ field_lat = int((latitude + 90) / 10)
+ grid = chr(ord('A') + field_lon) + chr(ord('A') + field_lat)
+
+ square_lon = int(((longitude + 180) % 20) / 2)
+ square_lat = int(((latitude + 90) % 10) / 1)
+ grid += str(square_lon) + str(square_lat)
+
+ subsquare_lon = int((((longitude + 180) % 20) % 2) / (2/24))
+ subsquare_lat = int((((latitude + 90) % 10) % 1) / (1/24))
+ grid += chr(ord('A') + subsquare_lon) + chr(ord('A') + subsquare_lat)
+
+ return grid
+
+latitude = 37.6872  # Example latitude
+longitude = -97.3301 # Example longitude
+
+grid_square = latlon_to_grid(latitude, longitude)
+print(f"{grid_square}")
